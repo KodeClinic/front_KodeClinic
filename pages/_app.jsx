7848809f4d37cb26d7f069b2f324234a87ea6f31
Ricardo Gomez <gduran.ricardo@gmail.com>
2024-03-23 00:41:52 -0600

    add font style Raleway as default

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
 
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={raleway.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
