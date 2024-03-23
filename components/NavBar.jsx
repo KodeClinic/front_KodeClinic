050ea9c93b2aaf3d85571ab92785a826bedcb9ea
Ricardo Gomez <gduran.ricardo@gmail.com>
2024-03-23 00:47:42 -0600

    add folders components & public/assets

---
+++ components/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <div>
+        <div>
+          <img src="img-kodeclinic-logo" alt="" />
+          KodeClinic
+        </div>
+      </div>
+    </nav>
+  );
+}
