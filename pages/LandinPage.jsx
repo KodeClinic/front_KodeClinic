24d36a6527eddbe20991fc657a48e290765ec07d
Ricardo Gomez <gduran.ricardo@gmail.com>
2024-03-25 00:28:17 -0600

    landing page navbar done

--- pages/LandinPage.jsx
+++ pages/LandinPage.jsx
@@ -2,7 +2,7 @@ import NavBar from "@/components/NavBar";
 
 export default function LandinPage() {
   return (
-    <main className="bg-background h-svh">
+    <main className="bg-background h-screen w-screen">
       <NavBar />
     </main>
   );
