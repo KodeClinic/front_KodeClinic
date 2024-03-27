eb8a2817aaa5f7c654a4a4c3d71bcedf34eff009
Hector <hectors.gpt@gmail.com>
2024-03-26 22:53:25 -0600

    Image added

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -2,9 +2,13 @@
 
 export default function Login() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h2>Hello </h2>
-    </div>
+    <main className="flex flex-col items-center text-center justify-center min-h-screen px-16 py-2 bg-white">
+        <div className="flex-col p-3 w-auto bg-blue_gray-300">{/* Sign in section */}
+        <image src="img-kodeclinic-logo" alt="KodeClinic logo" />
+
+
+        </div>
+    </main>
 
   );
 }
