5681f81f6634b450da910840f536b3ef89164465
Hector <hectors.gpt@gmail.com>
2024-03-27 20:37:30 -0600

    Structure created

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -2,13 +2,56 @@
 
 export default function Login() {
   return (
-    <main className="flex flex-col items-center text-center justify-center min-h-screen px-16 py-2 bg-white">
-        <div className="flex-col p-3 w-auto bg-blue_gray-300">{/* Sign in section */}
-        <image src="img-kodeclinic-logo" alt="KodeClinic logo" />
-
-
+    <div className="flex h-screen justify-center items-center">
+      <div className="w-full max-w-sm">
+        <div className="bg-white shadow-md rounded px-8 py-10 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <h1 className="text-2xl font-bold text-center">
+              Iniciar<span className="text-blue-500"> Sesión</span>
+            </h1>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm  mb-2">
+              Correo electrónico
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm  mb-2">
+              Contraseña
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+            />
+            <p className="text-red-500 text-xs italic">¿Olvidaste tu contraseña?</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue_button w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Ingresar
+            </button>
+          </div>
         </div>
-    </main>
-
+        <div className="bg-gray-100 rounded-b flex flex-col w-full p-4 items-center justify-between">
+          <p className="text-gray-600 text-sm">
+            ¿Aún no tienes una cuenta?
+            <a
+              className="text-blue_button font-bold hover:underline ml-2"
+              href="#"
+            >
+              Registrate
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
