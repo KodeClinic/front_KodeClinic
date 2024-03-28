87492d72fe1f161451a1b274f0dd6db5010684ee
Joel Martinez <joematz@live.com>
2024-03-27 20:28:56 -0600

    Adding icons and part of mobil design

--- components/Card_function.jsx
+++ components/Card_function.jsx
@@ -1,12 +1,12 @@
-import medical from "../public/assets/icons/medical_exp.svg";
-
-export default function Card_function() {
+export default function Card_function(props) {
   return (
-    <main className="flex justify-center items-center">
-      <article className="w-36 h-56 bg-white rounded-3xl shadow-2xl p-5 flex flex-col justify-center items-center">
-        <img className="w-12 h-12" src={medical} alt="MDN" />
-        <h1 className="text-[14px]">TITULO</h1>
-        <p className="text-[14px]">DESCRIPCION DE LA FUNCIONALIDAD</p>
+    <main className="flex justify-center items-center mt-5">
+      <article className="w-[159px] h-[223px] bg-white rounded-3xl shadow-2xl p-2 flex flex-col justify-center items-center">
+        <img className="w-12 h-12" src={props.icon} alt="MDN" />
+        <h1 className="font-bold text-[14px] mt-2">{props.title}</h1>
+        <p className="text-blue_gray-600 text-[14px] mt-3">
+          {props.description}
+        </p>
       </article>
     </main>
   );
--- pages/LandinPage.jsx
+++ pages/LandinPage.jsx
@@ -1,41 +1,100 @@
 import Card_function from "@/components/Card_function";
+import clsx from "clsx";
 
 export default function LandinPage() {
   return (
     <main>
       <section className="bg-background p-4">
-        <h1 className=" text-green_title text-[20px] font-semibold text-center p-4">
+        <h1
+          className={clsx(
+            "text-green_title font-semibold text-center p-4",
+            "sm:text-[20px] md:text-[38px]"
+          )}
+        >
           Funcionalidades
         </h1>
-        <p className="text-[14px] text-center p-4">
-          KodeClinic cuenta con funciones especializadas en la gestión de
-          documentación médica para que saques el mejor provecho a tu
-          Consultorio
-        </p>
-        <Card_function className="p-4" />
+        <span
+          className={clsx(
+            "text-center text-[#0745CB]",
+            "sm:text-[14px] md:text-[18px] lg:text-[22px]"
+          )}
+        >
+          Kode
+        </span>
+        <span
+          className={clsx(
+            "text-center text-[#0745CB] font-bold",
+            "sm:text-[14px] md:text-[18px] lg:text-[22px]"
+          )}
+        >
+          Clinic{" "}
+        </span>
+        <span
+          className={clsx(
+            "text-center text-blue_gray-600",
+            "sm:text-[14px] md:text-[18px] lg:text-[22px]"
+          )}
+        >
+          cuenta con funciones especializadas en la gestión de documentación
+          médica para que saques el mejor provecho a tu Consultorio
+        </span>
+
+        <div>
+          <Card_function
+            icon="assets/icons/medical_exp.svg"
+            title="Expediente Medico"
+            description="Digitaliza el Expediente Medico de tus pacientes y accede a él en cualquier momento"
+            className="p-4"
+          />
+        </div>
       </section>
 
-      <section className="bg-white p-4">
+      <section className="bg-white p-4 mt-3">
         <div className="grid grid-cols-2 justify-center">
           <div>
             <h1 className="text-green_title font-bold text-[14px]">
               Navegacion
             </h1>
+
             <p className="text-[12px]">Inicio</p>
             <p className="text-[12px]">Nosotros</p>
             <p className="text-[12px]">Funcionalidades</p>
           </div>
           <div>
             <h1 className="text-green_title font-bold">Contacto</h1>
-            <p className="text-[12px]">5513044788</p>
-            <p className="text-[12px]">kodeclinic@mail.com</p>
-            <p className="text-[12px]">26, Libertad, CDMX</p>
+            <p className="text-[12px] flex gap-2">
+              <span>
+                <img src="assets/icons/telephone.svg" alt="" />
+              </span>
+              5513044788
+            </p>
+            <p className="text-[12px] flex gap-2">
+              <span>
+                <img src="assets/icons/envelope.svg" alt="" />
+              </span>
+              kodeclinic@mail.com
+            </p>
+            <p className="text-[12px] flex gap-2">
+              <span>
+                <img src="assets/icons/geo-alt.svg" alt="" />
+              </span>
+              26, Libertad, CDMX
+            </p>
           </div>
         </div>
 
-        <div className="flex flex-col items-center mt-4 gap-2">
+        <div
+          className={clsx(
+            "flex items-center mt-4 gap-2",
+            "sm:flex-col md:flex-row lg:flex-row lg:justify-center"
+          )}
+        >
           <input
-            className="w-[328px] h-[48px] text-[12px] border-2 rounded-full border-[#0745CB] font-bold placeholder:text-[#0745CB] placeholder:text-center"
+            className={clsx(
+              "border-2 rounded-full border-[#0745CB] font-bold placeholder:text-[#0745CB] placeholder:text-center h-[48px] text-[13px]",
+              "sm:w-[328px]",
+              "lg:w-[353px]"
+            )}
             type="mail"
             name=""
             id=""
@@ -47,13 +106,21 @@ export default function LandinPage() {
         </div>
 
         <div className="mt-4">
-          <h1 className="font-bold text-[20px]">Kodeclinic</h1>
-          <p className="text-[14px]">
+          <span className="text-green_title text-[20px]">Kode</span>
+          <span className="text-green_title text-[20px] font-bold">Clinic</span>
+          <p className="text-[14px] mt-4 text-blue_gray-600">
             Nuestra plataforma facilita la organización de expedientes médicos y
             la gestión de citas para terapeutas. Contáctanos para más
             información. ¡Gracias por confiar en nosotros para cuidar de tu
             salud!
           </p>
+        </div>
+        <div className="flex gap-6 mt-4">
+          <img src="assets/icons/facebook.svg" alt="" />
+          <img src="assets/icons/twitter.svg" alt="" />
+          <img src="assets/icons/google.svg" alt="" />
+          <img src="assets/icons/instagram.svg" alt="" />
+          <img src="assets/icons/whatsapp.svg" alt="" />
         </div>
       </section>
     </main>
