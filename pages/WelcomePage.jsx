84da8c98110e4e4f494a8cc02c14fdbeb0f643c6
Karla Herzo ⚡+ <127571238+kherzo99@users.noreply.github.com>
2024-03-27 21:01:13 -0600

    mui featured

---
+++ pages/WelcomePage.jsx
@@ -0,0 +1,10 @@
+import ModalCard from "@/components/ModalCard";
+import clsx from "clsx";
+
+export default function WelcomePage() {
+  return (
+    <main className={clsx("bg-auto", "bg-black")}>
+      <ModalCard> </ModalCard>
+    </main>
+  );
+}
