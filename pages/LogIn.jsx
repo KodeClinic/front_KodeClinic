a76c70ed80a245fcadc79bd0b657b6b0471cbeb5
Hector <hectors.gpt@gmail.com>
2024-03-30 01:28:44 -0600

    Password Changed component created

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -2,9 +2,10 @@ import Login from "@/components/Login";
 import RecoveryEmail from "@/components/RecoveryEmail";
 import VerificationCode from "@/components/VerificationCode";
 import NewPassword from "@/components/NewPassword";
+import PasswordChaged from "@/components/PasswordChanged";
 
 export default function LoginPage() {
   return (
-    <NewPassword/>
+    <PasswordChaged/>
   );
 }
