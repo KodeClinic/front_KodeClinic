a562b7c801771bb94ad8ec241c5d10ac3b458d9f
Hector <hectors.gpt@gmail.com>
2024-03-30 00:32:18 -0600

    RecoveryEmail component created

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -1,8 +1,9 @@
 import Login from "@/components/Login";
+import RecoveryEmail from "@/components/recoveryEmail";
 
 export default function LoginPage() {
   return (
-    <Login />
+    <RecoveryEmail/>
   );
 }
 
