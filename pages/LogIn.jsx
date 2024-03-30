98b16baa63d1dfbb54671bc275f30be75599f158
Hector <hectors.gpt@gmail.com>
2024-03-30 01:05:20 -0600

    New password component created

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -1,10 +1,11 @@
 import Login from "@/components/Login";
 import RecoveryEmail from "@/components/RecoveryEmail";
-import VerificationCode from "@/components/verificationCode";
+import VerificationCode from "@/components/VerificationCode";
+import NewPassword from "@/components/NewPassword";
 
 export default function LoginPage() {
   return (
-    <VerificationCode/>
+    <NewPassword/>
   );
 }
 
