50eea48569eb2df85a415e9970473bd543adce5d
Hector <hectors.gpt@gmail.com>
2024-03-30 18:25:39 -0600

    Registration Complete component created

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -5,10 +5,11 @@ import NewPassword from "@/components/NewPassword";
 import PasswordChaged from "@/components/PasswordChanged";
 import Registration from "@/components/Registration";
 import VerificationEmail from "@/components/VerificationEmail";
+import RegistrationComplete from "@/components/RegistrationComplete";
 
 export default function LoginPage() {
   return (
-    <VerificationEmail/>
+    <RegistrationComplete/>
   );
 }
 
