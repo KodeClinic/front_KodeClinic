5b6c02fcbe85b9235904e438cd4e687da5673a30
Joel Martinez <joematz@live.com>
2024-04-01 22:49:40 -0600

    Changes for login

--- pages/LogIn.jsx
+++ pages/LogIn.jsx
@@ -1,10 +1,10 @@
-// import Login from "@/components/Login";
-// import RecoveryEmail from "@/components/RecoveryEmail";
-// import VerificationCode from "@/components/VerificationCode";
-// import NewPassword from "@/components/NewPassword";
-// import PasswordChaged from "@/components/PasswordChanged";
-// import Registration from "@/components/Registration";
-// import VerificationEmail from "@/components/VerificationEmail";
+import Login from "@/components/Login";
+import RecoveryEmail from "@/components/recoveryEmail";
+import VerificationCode from "@/components/verificationCode";
+import NewPassword from "@/components/NewPassword";
+import PasswordChaged from "@/components/PasswordChanged";
+import Registration from "@/components/Registration";
+import VerificationEmail from "@/components/VerificationEmail";
 import RegistrationComplete from "@/components/RegistrationComplete";
 
 export default function LoginPage() {
