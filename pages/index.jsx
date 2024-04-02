1ae60f31fa241eaa81b9369a674525229fc9bace
Joel Martinez <joematz@live.com>
2024-04-02 13:27:26 -0600

    Fixing landing

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,13 @@
-
 import LoginPage from "./LogIn";
 import Login from "./LogIn";
 import DashboardSpe from "./DashboardSpe";
 import DashboardPat from "./DashboardPat";
+import LandinPage from "./LandinPage";
 
 export default function Home() {
   return (
     <main>
-      <LoginPage />
+      <LandinPage />
     </main>
   );
 }
