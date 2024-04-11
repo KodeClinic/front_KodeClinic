98a2b69eac7a48565ad1fc014e22d7a597d56ffd
Hector <hectors.gpt@gmail.com>
2024-04-10 19:54:46 -0600

    Fix details patient infomration

--- components/EditInformation.jsx
+++ components/EditInformation.jsx
@@ -5,11 +5,11 @@ export default function EditInformation({ isVisible }) {
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center ">
-            <div className="px-4 bg-white flex flex-col w-[328px] rounded-[20px] lg:w-[800px] lg:[870px]">
+            <div className="px-3 bg-white flex flex-col w-[328px] rounded-[20px] lg:w-[800px] lg:[870px]">
                 <div className="flex-col flex text-center items-center ">
                     <div className="max-h-[500px]  overflow-y-auto lg:w-full ">
                         <div className="flex justify-end w-full mt-2">
-                            <img src="assets/icons/Close-icon.png" alt="Close" className="w-[20px] h-[20px] absolute " />
+                            <img src="assets/icons/Close-icon.png" alt="Close" className="w-[20px] h-[20px] absolute" />
                         </div>
                         <div className="text-center">
                             <p className='text-2xl
