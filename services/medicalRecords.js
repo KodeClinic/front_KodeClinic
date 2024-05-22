aa98ca1a5b71eef333b6ba0d9114f905bd3a3a32
Ricardo Gomez <gduran.ricardo@gmail.com>
2024-05-21 23:05:21 -0600

    adding process.env on every URI_BASE

--- services/medicalRecords.js
+++ services/medicalRecords.js
@@ -2,7 +2,7 @@
 
 export function postRecordsData(credentials) {
   const { patientId, templateId, data, token } = credentials;
-  const URL = `${URL_BASE}api/medicalRecords/create/${patientId}/${templateId}`;
+  const URL = `${process.env.URL_BASE}api/medicalRecords/create/${patientId}/${templateId}`;
   const options = {
     method: "POST",
     body: JSON.stringify(data),
@@ -13,7 +13,7 @@ export function postRecordsData(credentials) {
 
 export function getRecordsData(credentials) {
   const { patientId, templateId, token } = credentials;
-  const URL = `${URL_BASE}api/medicalRecords/get/${patientId}/${templateId}`;
+  const URL = `${process.env.URL_BASE}api/medicalRecords/get/${patientId}/${templateId}`;
   const options = {
     method: "GET",
     headers: { "Content-Type": "application/json", bearerauth: token },
