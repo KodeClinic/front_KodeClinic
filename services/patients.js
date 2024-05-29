cd78b5a5b4e4d4470a1d5be71d1aa94cb25d182b
Hector <hectors.gpt@gmail.com>
2024-05-28 19:39:52 -0600

    C:/Program Files/Git/ added

--- services/patients.js
+++ services/patients.js
@@ -2,7 +2,7 @@ import { URL_BASE } from "./config";
 
 export function pxInformation(credentials) {
   const { patientId, token } = credentials;
-  const URL = `${URL_BASE}api/users/patientList/${patientId}`;
+  const URL = `${URL_BASE}/api/users/patientList/${patientId}`;
   const options = {
     method: "GET",
     headers: { "Content-Type": "application/json", bearerauth: token },
