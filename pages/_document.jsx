3c7139df0528955e681899755fe14db15d667720
Ricardo Gomez <gduran.ricardo@gmail.com>
2024-06-01 18:46:18 -0600

    Stable version before endpoints restructuring

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+//Stable version before endpoints restructuring 1
 export default function Document() {
   return (
     <Html lang="en">
